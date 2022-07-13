ba31a20492f50e24f9cfda709ea2d109faa26267
Jason Ho <jason.ho@obf.ateneo.edu>
2022-07-13 15:29:34 +0800

    Separate templates

--- webkiosk/views.py
+++ webkiosk/views.py
@@ -12,16 +12,16 @@ def index(request):
 def foods_index(request):
     foods = Food.objects.all()
     context = {'foods': foods}
-    return render(request, 'webkiosk/foods_index.html', context)
+    return render(request, 'webkiosk/foods/index.html', context)
 
 
 def customers_index(request):
     customers = Customer.objects.all()
     context = {'customers': customers}
-    return render(request, 'webkiosk/customers_index.html', context)
+    return render(request, 'webkiosk/customers/index.html', context)
 
 
 def orders_index(request):
     orders = Order.objects.all()
     context = {'orders': orders}
-    return render(request, 'webkiosk/orders_index.html', context)
+    return render(request, 'webkiosk/orders/index.html', context)
