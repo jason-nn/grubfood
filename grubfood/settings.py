36738890c9919b419cd02b43adc7d3645aea8b46
Jason Ho <jason.ho@obf.ateneo.edu>
2022-07-13 11:28:03 +0800

    Add models

--- grubfood/settings.py
+++ grubfood/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webkiosk.apps.WebkioskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- webkiosk/models.py
+++ webkiosk/models.py
@@ -1,3 +1,37 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name}, {self.description}, {self.price}'
+
+
+class Customer(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.firstname} {self.lastname}, {self.address}, {self.city}'
+
+
+class Order(models.Model):
+    PAYMENT_CHOICES = [
+        ('Cash', 'Cash'),
+        ('Card', 'Card'),
+    ]
+
+    orderdatetime = models.DateTimeField(default=datetime.now())
+    paymentmode = models.CharField(max_length=4, choices=PAYMENT_CHOICES)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.customer}, {self.food}, {self.quantity}, {self.orderdatetime}'
