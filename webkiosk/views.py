0ccb9f155557a74bf2fde38dbb7314b452c1af91
Jason Ho <jason.ho@obf.ateneo.edu>
2022-07-13 15:59:20 +0800

    Add show pages

--- webkiosk/views.py
+++ webkiosk/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
@@ -15,13 +15,31 @@ def foods_index(request):
     return render(request, 'webkiosk/foods/index.html', context)
 
 
+def foods_show(request, food_id):
+    food = get_object_or_404(Food, pk=food_id)
+    context = {'food': food}
+    return render(request, 'webkiosk/foods/show.html', context)
+
+
 def customers_index(request):
     customers = Customer.objects.all()
     context = {'customers': customers}
     return render(request, 'webkiosk/customers/index.html', context)
 
 
+def customers_show(request, customer_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+    context = {'customer': customer}
+    return render(request, 'webkiosk/customers/show.html', context)
+
+
 def orders_index(request):
     orders = Order.objects.all()
     context = {'orders': orders}
     return render(request, 'webkiosk/orders/index.html', context)
+
+
+def orders_show(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    context = {'order': order}
+    return render(request, 'webkiosk/orders/show.html', context)
