7ef2e55fbaea8a808b44465435b7362acdeb16a0
Jason Ho <jason.ho@obf.ateneo.edu>
2022-07-13 15:27:26 +0800

    Add index pages

--- webkiosk/urls.py
+++ webkiosk/urls.py
@@ -4,4 +4,35 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('foods', views.foods_index, name='foods_index'),
+    # path('foods/new', views.foods_new, name='foods_new'),
+    # path('foods/create', views.foods_create, name='foods_create'),
+    # path('foods/<int:food_id>', views.foods_show, name='foods_show'),
+    # path('foods/<int:food_id>/edit', views.foods_edit, name='foods_edit'),
+    # path('foods/<int:food_id>/update', views.foods_edit, name='foods_update'),
+    # path('foods/<int:food_id>/destroy',
+    #      views.foods_destroy, name='foods_destroy'),
+
+    path('customers', views.customers_index, name='customers_index'),
+    # path('customers/new', views.customers_new, name='customers_new'),
+    # path('customers/create', views.customers_create, name='customers_create'),
+    # path('customers/<int:customer_id>',
+    #      views.customers_show, name='customers_show'),
+    # path('customers/<int:customer_id>/edit',
+    #      views.customers_edit, name='customers_edit'),
+    # path('customers/<int:customer_id>/update',
+    #      views.customers_edit, name='customers_update'),
+    # path('customers/<int:customer_id>/destroy',
+    #      views.customers_destroy, name='customers_destroy'),
+
+    path('orders', views.orders_index, name='orders_index'),
+    # path('orders/new', views.orders_new, name='orders_new'),
+    # path('orders/create', views.orders_create, name='orders_create'),
+    # path('orders/<int:order_id>', views.orders_show, name='orders_show'),
+    # path('orders/<int:order_id>/edit', views.orders_edit, name='orders_edit'),
+    # path('orders/<int:order_id>/update',
+    #      views.orders_edit, name='orders_update'),
+    # path('orders/<int:order_id>/destroy',
+    #      views.orders_destroy, name='orders_destroy'),
 ]
--- webkiosk/views.py
+++ webkiosk/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Food, Customer, Order
+
 
 def index(request):
-    return HttpResponse('Webkiosk index')
+    return HttpResponse('Home')
+
+
+def foods_index(request):
+    foods = Food.objects.all()
+    context = {'foods': foods}
+    return render(request, 'webkiosk/foods_index.html', context)
+
+
+def customers_index(request):
+    customers = Customer.objects.all()
+    context = {'customers': customers}
+    return render(request, 'webkiosk/customers_index.html', context)
+
+
+def orders_index(request):
+    orders = Order.objects.all()
+    context = {'orders': orders}
+    return render(request, 'webkiosk/orders_index.html', context)
